a6ef2041ccbd61cef13e2d0445bd1fea86f8e489
vcolin7 <victor.y.asi@gmail.com>
2025-02-18 10:43:34 -0800

    Fixed more package naming issues

--- sdk/storage/azure-storage-blob-stress-v2/src/main/java/com/azure/v2/storage/blob/stress/App.java
+++ sdk/storage/azure-storage-blob-stress-v2/src/main/java/com/azure/v2/storage/blob/stress/App.java
@@ -4,7 +4,7 @@
 package com.azure.v2.storage.blob.stress;
 
 import com.azure.perf.test.core.PerfStressProgram;
-import com.azure.storage.stress.TelemetryHelper;
+import com.azure.v2.storage.stress.TelemetryHelper;
 
 public class App {
     public static void main(String[] args) {
