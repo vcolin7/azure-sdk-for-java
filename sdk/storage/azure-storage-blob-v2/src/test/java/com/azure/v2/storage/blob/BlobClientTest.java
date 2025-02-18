851a584b87b57d688fee4dd584cdabcc3f20310c
vcolin7 <victor.y.asi@gmail.com>
2025-02-18 10:42:41 -0800

    Updated azure-storage-blob-v2 tests.

--- sdk/storage/azure-storage-blob-v2/src/test/java/com/azure/v2/storage/blob/BlobClientTest.java
+++ sdk/storage/azure-storage-blob-v2/src/test/java/com/azure/v2/storage/blob/BlobClientTest.java
@@ -3,24 +3,33 @@ package com.azure.v2.storage.blob;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class BlobClientTest {
-
     @Test
-    @Disabled
     public void testDownload() throws IOException {
+        BlobClient blobClient = new AzureBlobStorageBuilder()
+            .url("sas-url")
+            .buildBlobClient();
 
-        BlobClient blobClient = new AzureBlobStorageBuilder().url("sas-url").buildBlobClient();
-
-        InputStream sampleText = blobClient.download("testcontainer", "sample.txt", null, null, null, null, null, null,
+        InputStream sampleText = blobClient.download("test-container", "upload.txt", null, null, null, null, null, null,
             null, null, null, null, null, null, null, null, null);
 
-        byte[] bytes = new byte[2048];
-        sampleText.read(bytes);
-        String text = new String(bytes);
+        String text = new String(readAllBytes(sampleText));
         System.out.println(text);
+    }
 
+    public static byte[] readAllBytes(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+
+        while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+
+        return buffer.toByteArray();
     }
 }
--- sdk/storage/azure-storage-blob-v2/src/test/java/com/azure/v2/storage/blob/ContainerClientTest.java
+++ sdk/storage/azure-storage-blob-v2/src/test/java/com/azure/v2/storage/blob/ContainerClientTest.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ContainerClientTest {
-
     @Test
     @Disabled
     public void testContainterClient() {
+        ContainerClient containerClient = new AzureBlobStorageBuilder()
+            .url("sas-url")
+            .buildContainerClient();
 
-        ContainerClient containerClient = new AzureBlobStorageBuilder().url("sas-url").buildContainerClient();
-
-        containerClient.create("container", null, null, null, null, null);
+        containerClient.create("test-container", null, null, null, null, null);
 
         // This will not work yet as xml serialization is not supported in clientcore.
         //        Response<ListBlobsHierarchySegmentResponse> testcontainer = containerClient.listBlobHierarchySegmentNoCustomHeadersWithResponse("testcontainer", null, null, null, null, null, null, null, null);
